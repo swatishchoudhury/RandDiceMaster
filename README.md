46ded95a62b5755acb724a63e02640b17d554cf0
Swatishchoudhury <swatish.choudhury@gmail.com>
2019-09-30 16:00:26 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dicee
+
+Most basic state managment app via flutter coz i keep getting confused.
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:math';
+
+void main() {
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ],
+  );
+
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Dicee'),
+          backgroundColor: Colors.black,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int diceNumber1 = 2;
+  int diceNumber2 = 2;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Container(
+            child: Expanded(
+              child: FlatButton(
+                child: Image.asset('images/dice$diceNumber1.png'),
+                onPressed: () {
+                  rand();
+                },
+              ),
+            ),
+          ),
+          Container(
+            child: Expanded(
+              child: FlatButton(
+                child: Image.asset('images/dice$diceNumber2.png'),
+                onPressed: () {
+                  rand();
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void rand() {
+    setState(() {
+      diceNumber1 = Random().nextInt(6) + 1;
+      diceNumber2 = Random().nextInt(6) + 1;
+    });
+  }
+}
